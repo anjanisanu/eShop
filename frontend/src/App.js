8a170c1f8555b8538248cbeb0ec35e20619eb49a
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-07 19:58:44 +0530

    Added React Bootstrap, font awesome to the project with Header & Footer on the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-	return <div>eShop</div>;
+	return (
+		<Fragment>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h2>eShop</h2>
+				</Container>
+			</main>
+			<Footer />
+		</Fragment>
+	);
 };
 
 export default App;
