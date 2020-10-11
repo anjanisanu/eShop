964b1e6058e43464cc3dafba1c42956579e402cd
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-11 21:38:16 +0530

    Added catchAsync function to catch error from async functions

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,27 +1,26 @@
 import express from 'express';
 import Product from './../models/productModel.js';
+import catchAsync from './../utils/catchAsync.js';
+import AppError from './../utils/appError.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-	const products = await Product.find();
-	res.json(products);
-});
+router.get(
+	'/',
+	catchAsync(async (req, res) => {
+		const products = await Product.find();
+		res.json(products);
+	})
+);
 
-router.get('/:id', async (req, res) => {
-	try {
+router.get(
+	'/:id',
+	catchAsync(async (req, res, next) => {
 		const product = await Product.findById(req.params.id);
-		if (!product)
-			return res.status(404).json({
-				message: 'Product Not Found'
-			});
+		if (!product) return next(new AppError('Product Not found', 404));
+
 		res.status(200).json(product);
-	} catch (err) {
-		res.status(401).json({
-			status: 'fail',
-			error: err.stack
-		});
-	}
-});
+	})
+);
 
 export default router;
