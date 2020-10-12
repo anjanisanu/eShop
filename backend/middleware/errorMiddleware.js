70c66c219384cf2fac1b7b96c22f09b26ed864fc
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-12 20:31:45 +0530

    Refactored errorHandler and handled cast error from mongoDB

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,8 +1,47 @@
+import AppError from './../utils/appError.js';
+
+const handleDbCastError = (err) => {
+	const message = `Invalid ${err.path}: ${err.value}`;
+	return new AppError(message, 400);
+};
+
+const sendErrDev = (err, res) => {
+	res.status(err.statusCode).json({
+		message: err.message,
+		error: err,
+		stack: err.stack
+	});
+};
+
+const sendErrProd = (err, res) => {
+	// Trusted Error: Send Error to client
+	if (err.isOperational) {
+		res.status(err.statusCode).json({
+			message: err.message
+		});
+		// Programming Error or any unknown error
+	} else {
+		// Log Error to console
+		console.log('Some Error Occured', err);
+		// Send generic error message
+		res.status(500).json({
+			message: 'Something went wrong'
+		});
+	}
+};
+
 const errorHandler = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
-	res.status(err.statusCode).json({
-		message: err.message
-	});
+
+	if (process.env.NODE_ENV === 'development') {
+		sendErrDev(err, res);
+	}
+
+	if (process.env.NODE_ENV === 'production') {
+		if (err.name === 'CastError') err = handleDbCastError(err);
+
+		sendErrProd(err, res);
+	}
 };
 
 export { errorHandler };
