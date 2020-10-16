1fb2dad8ebfd33ead5b1a2652fac64bdfe992ff7
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-16 22:41:29 +0530

    Added logout function and changed navigation after user logs in

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from './../constants/userConstants';
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT } from './../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
 	try {
@@ -25,4 +25,9 @@ export const login = (email, password) => async (dispatch) => {
 			payload: err.response && err.response.data.message ? err.response.data.message : err.message
 		});
 	}
+};
+
+export const logout = () => (dispatch) => {
+	localStorage.removeItem('userInfo');
+	dispatch({ type: USER_LOGOUT });
 };
