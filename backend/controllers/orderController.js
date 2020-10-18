3c1c2f8936488bbbf7e3c7283672459c714dacda
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-18 18:38:00 +0530

    Added order routes and controller in the backend

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,26 @@
+import Order from './../models/orderModel.js';
+import catchAsync from './../utils/catchAsync.js';
+import AppError from './../utils/appError.js';
+
+export const addOrderItems = catchAsync(async (req, res, next) => {
+	const { orderItems, shippingAddress, paymentAddress, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+
+	if (orderItems && orderItems.length === 0) return next(new AppError('No Order Items', 400));
+
+	const order = new Order({
+		orderItems,
+		user: req.user._id,
+		shippingAddress,
+		paymentAddress,
+		itemsPrice,
+		taxPrice,
+		shippingPrice,
+		totalPrice
+	});
+
+	const createdOrder = await order.save();
+
+	res.status(201).json({
+		createdOrder
+	});
+});
