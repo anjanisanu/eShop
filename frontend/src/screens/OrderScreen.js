2c342d4598025040d93a8ece625349bb736569d0
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-19 18:20:08 +0530

    Added Payemnt from paypal in the front end

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,18 +1,26 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+import { PayPalButton } from 'react-paypal-button-v2';
 import { Row, Col, Card, Button, ListGroup, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from './../components/Loader';
 import Message from './../components/Message';
-import { getOrderDetails } from './../actions/orderActions';
+import { getOrderDetails, payOrder } from './../actions/orderActions';
+import { ORDER_PAY_RESET } from './../constants/orderConstants';
 
 const OrderScreen = ({ match }) => {
 	const orderId = match.params.id;
+
+	const [ sdkReady, setSdkReady ] = useState(false);
 
 	const dispatch = useDispatch();
 
 	const orderDetails = useSelector((state) => state.orderDetails);
 	const { order, loading, error } = orderDetails;
+
+	const orderPay = useSelector((state) => state.orderPay);
+	const { loading: loadingPay, success: successPay } = orderPay;
 
 	if (!loading) {
 		//Calculate Price
@@ -25,12 +33,34 @@ const OrderScreen = ({ match }) => {
 
 	useEffect(
 		() => {
-			if (!order || order._id !== orderId) {
+			const addPayPalScript = async () => {
+				const { data: clientId } = await axios.get('/api/config/paypal');
+				const script = document.createElement('script');
+				script.type = 'text/javascript';
+				script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
+				script.async = true;
+				script.onload = () => {
+					setSdkReady(true);
+				};
+				document.body.appendChild(script);
+			};
+
+			if (!order || successPay) {
+				dispatch({ type: ORDER_PAY_RESET });
 				dispatch(getOrderDetails(orderId));
+			} else if (!order.isPaid) {
+				addPayPalScript();
+			} else {
+				setSdkReady(true);
 			}
 		},
-		[ order, orderId ]
+		[ dispatch, order, orderId, successPay ]
 	);
+
+	const successPaymentHandler = (paymentResult) => {
+		console.log(paymentResult);
+		dispatch(payOrder(orderId, paymentResult));
+	};
 
 	return loading ? (
 		<Loader />
@@ -110,27 +140,37 @@ const OrderScreen = ({ match }) => {
 							<ListGroup.Item>
 								<Row>
 									<Col>Items</Col>
-									<Col>${order.itemsPrice}</Col>
+									<Col>INR{order.itemsPrice}</Col>
 								</Row>
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Row>
 									<Col>Shipping</Col>
-									<Col>${order.shippingPrice}</Col>
+									<Col>INR{order.shippingPrice}</Col>
 								</Row>
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Row>
 									<Col>Tax</Col>
-									<Col>${order.taxPrice}</Col>
+									<Col>INR{order.taxPrice}</Col>
 								</Row>
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Row>
 									<Col>Total</Col>
-									<Col>${order.totalPrice}</Col>
+									<Col>INR{order.totalPrice}</Col>
 								</Row>
 							</ListGroup.Item>
+							{!order.isPaid && (
+								<ListGroup.Item>
+									{loadingPay && <Loader />}
+									{!sdkReady ? (
+										<Loader />
+									) : (
+										<PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler} />
+									)}
+								</ListGroup.Item>
+							)}
 						</ListGroup>
 					</Card>
 				</Col>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,7 +10,7 @@ import {
 	userDetiailsReducer,
 	userUpdateProfileReducer
 } from './reducers/userReducers';
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducers';
 
 const reducer = combineReducers({
 	productList: productReducer,
@@ -21,7 +21,8 @@ const reducer = combineReducers({
 	userDetails: userDetiailsReducer,
 	userUpdateProfile: userUpdateProfileReducer,
 	orderCreate: orderCreateReducer,
-	orderDetails: orderDetailsReducer
+	orderDetails: orderDetailsReducer,
+	orderPay: orderPayReducer
 });
 
 const cartItemFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
