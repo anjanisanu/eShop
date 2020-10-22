10152d1e5eec4a0e22728daa75f3308b254f5b53
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-22 16:25:38 +0530

    Added a dialogue box before deleting a user

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -32,7 +32,9 @@ const UserListScreen = ({ history }) => {
 	);
 
 	const deleteHandler = (id) => {
-		dispatch(deleteUser(id));
+		if (window.confirm('Are you sure?')) {
+			dispatch(deleteUser(id));
+		}
 	};
 
 	return (
