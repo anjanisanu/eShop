99afaebd6d60caa9296455b9d916e2ddb208e09e
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-22 15:51:44 +0530

    Added useFindAndModify to false while connecting to database

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
 		const con = await mongoose.connect(process.env.MONGO_URI, {
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
-			useCreateIndex: true
+			useCreateIndex: true,
+			useFindAndModify: false
 		});
 
 		console.log(`MongoDB Connected: ${con.connection.host}`);
