e95d6eb7e809d8eb312040179ca4d83895354f45
Anjani Sanu <anjanikrsanu@gmail.com>
2020-10-25 17:13:51 +0530

    Admin Function: added a button for paid orders to mark it as delivered

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -12,6 +12,9 @@ import {
 	ORDER_PAY_REQUEST,
 	ORDER_PAY_SUCCESS,
 	ORDER_PAY_FAIL,
+	ORDER_DELIVER_REQUEST,
+	ORDER_DELIVER_SUCCESS,
+	ORDER_DELIVER_FAIL,
 	ORDER_MY_LIST_REQUEST,
 	ORDER_MY_LIST_SUCCESS,
 	ORDER_MY_LIST_FAIL
@@ -92,6 +95,33 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
 	} catch (err) {
 		dispatch({
 			type: ORDER_PAY_FAIL,
+			payload: err.response && err.response.data.message ? err.response.data.message : err.message
+		});
+	}
+};
+
+export const deliverOrder = (order) => async (dispatch, getState) => {
+	try {
+		dispatch({ type: ORDER_DELIVER_REQUEST });
+
+		const { userLogin: { userInfo } } = getState();
+
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`
+			}
+		};
+
+		const { data } = await axios.put(`/api/orders/${order._id}/deliver`, {}, config);
+
+		dispatch({
+			type: ORDER_DELIVER_SUCCESS,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: ORDER_DELIVER_FAIL,
 			payload: err.response && err.response.data.message ? err.response.data.message : err.message
 		});
 	}
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,26 +1,32 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import { PayPalButton } from 'react-paypal-button-v2';
-import { Row, Col, Card, ListGroup, Image } from 'react-bootstrap';
+import { Row, Col, Card, ListGroup, Image, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from './../components/Loader';
 import Message from './../components/Message';
-import { getOrderDetails, payOrder } from './../actions/orderActions';
-import { ORDER_PAY_RESET } from './../constants/orderConstants';
+import { getOrderDetails, payOrder, deliverOrder } from './../actions/orderActions';
+import { ORDER_PAY_RESET, ORDER_DELIVER_RESET } from './../constants/orderConstants';
 
-const OrderScreen = ({ match }) => {
+const OrderScreen = ({ match, history }) => {
 	const orderId = match.params.id;
 
 	const [ sdkReady, setSdkReady ] = useState(false);
 
 	const dispatch = useDispatch();
 
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
 	const orderDetails = useSelector((state) => state.orderDetails);
 	const { order, loading, error } = orderDetails;
 
 	const orderPay = useSelector((state) => state.orderPay);
 	const { loading: loadingPay, success: successPay } = orderPay;
+
+	const orderDeliver = useSelector((state) => state.orderDeliver);
+	const { loading: loadingDeliver, success: successDeliver } = orderDeliver;
 
 	if (!loading) {
 		//Calculate Price
@@ -33,6 +39,10 @@ const OrderScreen = ({ match }) => {
 
 	useEffect(
 		() => {
+			if (!userInfo) {
+				history.push('/login');
+			}
+
 			const addPayPalScript = async () => {
 				const { data: clientId } = await axios.get('/api/config/paypal');
 				const script = document.createElement('script');
@@ -45,8 +55,10 @@ const OrderScreen = ({ match }) => {
 				document.body.appendChild(script);
 			};
 
-			if (!order || successPay) {
+			if (!order || successPay || successDeliver) {
 				dispatch({ type: ORDER_PAY_RESET });
+				dispatch({ type: ORDER_DELIVER_RESET });
+
 				dispatch(getOrderDetails(orderId));
 			} else if (!order.isPaid) {
 				addPayPalScript();
@@ -54,12 +66,16 @@ const OrderScreen = ({ match }) => {
 				setSdkReady(true);
 			}
 		},
-		[ dispatch, order, orderId, successPay ]
+		[ dispatch, order, orderId, successPay, successDeliver, userInfo, history ]
 	);
 
 	const successPaymentHandler = (paymentResult) => {
 		console.log(paymentResult);
 		dispatch(payOrder(orderId, paymentResult));
+	};
+
+	const deliverHandler = () => {
+		dispatch(deliverOrder(order));
 	};
 
 	return loading ? (
@@ -169,6 +185,19 @@ const OrderScreen = ({ match }) => {
 									) : (
 										<PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler} />
 									)}
+								</ListGroup.Item>
+							)}
+
+							{loadingDeliver && <Loader />}
+
+							{userInfo &&
+							userInfo.isAdmin &&
+							order.isPaid &&
+							!order.isDelivered && (
+								<ListGroup.Item>
+									<Button type='button' className='btn btn-block' onClick={deliverHandler}>
+										Mark as Delivered
+									</Button>
 								</ListGroup.Item>
 							)}
 						</ListGroup>
